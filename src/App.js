5bc1105309acaa9a36ff53a3880528d2253b152e
Alexander <AlexanderRy@protonmail.com>
2025-03-23 01:54:47 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Products from './Components/Products/Products';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     <Products></Products>
-    </div>
+
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Products />} />
+      {/* <Route path="/cart" element={<Cart />} />
+      <Route path="/login" element={<Login />} /> */}
+    </Routes>
+  </BrowserRouter>
+
+   
   );
 }
 
